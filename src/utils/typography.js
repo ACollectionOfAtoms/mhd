03c1a138a8eeff1b03096713884858051c74d046
Adam <davidatomhernandez@gmail.com>
2018-04-28 14:21:09 -0400

    update typography; colors

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,11 @@
 import Typography from "typography";
 import moraga from "typography-theme-moraga";
 
-const typography = new Typography(moraga);
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['IBM Plex Serif', 'serif'],
+  bodyFontFamily: ['Raleway', 'sans-serif'],
+})
 
 export default typography;
