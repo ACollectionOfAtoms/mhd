63ecebed2444af1d71603a166272ead1b3a00b00
Adam <davidatomhernandez@gmail.com>
2018-04-28 16:15:10 -0400

    add intro section

--- src/components/IntroSlide/index.js
+++ src/components/IntroSlide/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from './styles.module.scss';
 
+import introImage from '../../img/introImg.png';
+
 export default class IntroSlide extends React.Component {
   render() {
     return (
       <section className={styles.introSection}>
-        <h1 className={styles.title}> the mental health directory </h1>
+        <header>
+          <h1 className={styles.title}> the mental health directory </h1>
+        </header>
+        <section className={styles.mainContent}>
+          <figure className={styles.intoFigure}>
+            <img src={introImage} />
+          </figure>
+          <section className={styles.blurb}>
+            <h1> Finding the Resources You Need For a Brighter Day Should’nt Be Hard </h1>
+          </section>
+        </section>
       </section>
     );
   }
