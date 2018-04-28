c4af1d1c7d0806a3051e111299f1182a7d9f2997
Adam <davidatomhernandez@gmail.com>
2018-04-28 14:21:22 -0400

    clear out old stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,36 +18,12 @@ export default class IndexPage extends React.Component {
   }
 
   render() {
-    const { data } = this.props;
-    const html = data.allMarkdownRemark.edges[0].node.html;
     return (
       <div>
-          <section className="hero is-medium is-primary is-bold">
-              <div className="hero-body">
-                <div className="container">
-                  <h1 className="title">
-                    Welcome to the
-                    <br/>
-                    Mental Health Directory
-                  </h1>
-                  <h2 className="subtitle">
-                    A site that will promote the destigmatization of mental health needs by creating relatable and compassionate access to mental health resources.
-                  </h2>
-                </div>
-              </div>
-            </section>
-          <section className="section">
-          <Script
-            url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-            onLoad={() => this.handleScriptLoad()}
-          />
-          <div className="container">
-            <div
-              className="about-page-content"
-              dangerouslySetInnerHTML={{ __html: html }}
-            />
-          </div>
-        </section>
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={() => this.handleScriptLoad()}
+        />
       </div>
     );
   }
