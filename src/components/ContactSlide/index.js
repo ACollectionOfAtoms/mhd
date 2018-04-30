52308e149f0489bfecf7929f69344e0116a71940
Adam <davidatomhernandez@gmail.com>
2018-04-30 18:58:58 -0400

    add contact-us section

--- src/components/ContactSlide/index.js
+++ src/components/ContactSlide/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
+import cx from 'classnames';
 import styles from './styles.module.scss';
 
-import brainImage from '../../img/brain.png';
-
 export default class ValueProp1Slide extends React.Component {
+    // See mixins.scss for MediaQuery widths!
     render() {
         return (
-            <section className={styles.valuePropSection}>
-                <header className={styles.header}>
-                    <h1 className={styles.title}> Let’s Shine Some Light on Mental Health Resources </h1>
-                </header>
-                <figure className={styles.figure}>
-                    <img src={brainImage} />
-                </figure>
+            <section className={styles.contactSection}>
+                <MediaQuery query='(max-width: 767px)'>
+                    <header className={styles.header}>
+                        <h1 className={styles.title}> Let’s Shine Some Light on Mental Health Resources </h1>
+                    </header>
+                </MediaQuery>
+                <section className={styles.faceSection}>
+                    <div className={styles.leftSide}>
+                        <div>
+                            <div className={cx(styles.megan, styles.face)}></div>
+                        </div>
+                        <div>
+                            <div className={cx(styles.adam, styles.face)}></div>
+                        </div>
+                    </div>
+                    <div className={styles.rightSide}>
+                        <div>
+                            <div className={cx(styles.vickie, styles.face)}></div>
+                        </div>
+                        <div>
+                            <div className={cx(styles.michael, styles.face)}></div>
+                        </div>
+                    </div>
+                </section>
                 <section className={styles.mainContent}>
+                    <MediaQuery query='(min-width: 768px)'>
+                        <h1 className={styles.title}> Let’s Shine Some Light on Mental Health Resources </h1>
+                    </MediaQuery> 
                     <p> The Mental Health Directory is in its initial stages and the team is open to any feedback and help that people may want to contribute. </p>
                     <p> We appreciate anything that may make the site helpful to more people. This could be anything from sharing a similar site that has worked for you, something that you hope to see in the directory, or web design and development help. </p>
                     <p> If you would like to offer your knowledge, or provide advice/feedback, please email us at:</p> 
