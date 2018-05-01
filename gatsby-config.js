61854efe7a827d41d0e030912a7bc1c861437aef
Adam <davidatomhernandez@gmail.com>
2018-04-30 21:20:17 -0400

    remove autoprefixer so that it works?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,71 +1,69 @@
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  siteMetadata: {
-    title: 'Mental Health Directory',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-114669150-1'
-      }
+    siteMetadata: {
+        title: 'Mental Health Directory',
     },
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography.js',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-postcss-sass`,
-      options: {
-        postCssPlugins: [autoprefixer()],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-react-css-modules`,
-      options: {
-        // *.css files are included by default.
-        // To support another syntax (e.g. SCSS),
-        // add `postcss-scss` to your project's devDependencies
-        // and add the following option here:
-        filetypes: {
-          ".scss": { syntax: `postcss-scss` },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-114669150-1'
+            }
         },
+        {
+            resolve: 'gatsby-plugin-typography',
+            options: {
+                pathToConfigModule: 'src/utils/typography.js',
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/img`,
+                name: 'images',
+            },
+        },
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [],
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-netlify-cms',
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-postcss-sass',
+            // Excluding Autoprefixer works results in prefixing?????
+        },
+        {
+            resolve: 'gatsby-plugin-react-css-modules',
+            options: {
+                // *.css files are included by default.
+                // To support another syntax (e.g. SCSS),
+                // add `postcss-scss` to your project's devDependencies
+                // and add the following option here:
+                filetypes: {
+                    '.scss': { syntax: 'postcss-scss' },
+                },
   
-        // Exclude global styles from the plugin using a RegExp:
-        exclude: `\/global\/`,
-      },
-    }
-  ],
+                // Exclude global styles from the plugin using a RegExp:
+                exclude: '\/global\/',
+            },
+        }
+    ],
 };
